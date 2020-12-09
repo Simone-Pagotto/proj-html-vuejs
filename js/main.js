5f69933f76fe076fb4e8d844a2c3f1c6381a5153
Simone <simone.pagotto.kg@gmail.com>
2020-12-09 16:01:37 +0100

    prime sezioni
    -header,footer: liste con vue
    -implementare icone con lista vue

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const myBakery_1 = new Vue ({
+    el:'#root_1',
+    data:{
+        headerNavbar : [
+            'Home',
+            'Shop',
+            'About',
+            'Gallery',
+            'Locations',
+            'Journal',
+            'Contact',
+            'My Account',
+            /* "<i class='fas fa-shopping-cart'></i>", */
+        ]
+    }
+})
+
+const myBakery_3 = new Vue ({
+    el:'#root_3',
+    data:{
+        headerFooter : [
+            'Shop',
+            'About',
+            'Gallery',
+            'Locations',
+            'Journal',
+            'Contact',
+            'Orders',
+            /* "<i class='fas fa-shopping-cart'></i>", */
+        ]
+    }
+})
